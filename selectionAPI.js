e14235704e97480a4a80386ff4748784c31cf5b0
SkinnyFatMan <SkinnyFatMan@gmail.com>
2015-09-14 11:25:30 -1000

    rougly function code

--- selectionAPI.js
+++ selectionAPI.js
@@ -1,7 +1,7 @@
 function runOOPMobiles(stringArray) {
   var newVehiculars = [];
   for(var i = 0; i < stringArray.length; i++) {
-    switch(stringArray[i]) {
+    switch(stringArray[i].toLowerCase()) {
       case 'full' :
         newVehiculars[i] = new Full('Dodge Status', 'red', 'manual', 'unleaded');
       break;
@@ -9,16 +9,16 @@ function runOOPMobiles(stringArray) {
         newVehiculars[i] = new Compact('Toyota Corolla', 'red', 5, 'manual', 'unleaded');
       break;
       case 'truck' :
-        newVehiculars[i] = new Truck();
+        newVehiculars[i] = new Truck('Chevrolet Avalanche', 'blue', '4', 8, 6, 'automatic', 'unleaded');
       break;
       case 'suv' :
-        newVehiculars[i] = new SUV();
+        newVehiculars[i] = new SUV('Honda CR-V', 'Orange', 4, 6, 7, 'manual', 'unleaded');
       break;
       case 'semi' :
-        newVehiculars[i] = new Semi();
+        newVehiculars[i] = new Semi('Peterbit 379', 'red', 6);
       break;
       case 'bus' :
-        newVehiculars[i] = new Bus();
+        newVehiculars[i] = new Bus('PartyBus', 'Neon Tyedye', 6, 'deisel');
       break;
       case false :
 
